9391347c127fb0c25c4d4fcad5d1e0e15fee24c5
Antti Vuorela <antti.vuorela@iki.fi>
2012-03-13 16:09:13 +0200

    Added Antti's UI test to playground

---
+++ playGround/DajktaUITestAndroid4/src/fi/aalto/dajkta/UItest/DajktaUITestAndroid4Activity.java
@@ -0,0 +1,22 @@
+package fi.aalto.dajkta.UItest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class DajktaUITestAndroid4Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+}
