1bf1c15b36bd8dd8308b6a695ea6694df79c2819
kranki <kalle.ranki@aalto.fi>
2012-03-23 09:08:12 +0200

    Keep Activity names as activity for consistency.

--- application/src/fi/local/social/network/MainActivity.java
+++ application/src/fi/local/social/network/MainActivity.java
@@ -12,7 +12,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class MainView extends Activity {
+public class MainActivity extends Activity {
 	private BroadcastReceiver mReceiver;
 
 	/** Called when the activity is first created. */
