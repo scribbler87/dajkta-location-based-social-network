966a0cf08ebf9b8ea0f0b87aaaff263362e23e7d
kranki <kalle.ranki@aalto.fi>
2012-05-05 13:13:08 +0300

    Removed feature to add pictures to events

--- application/src/fi/local/social/network/activities/NewEventActivity.java
+++ application/src/fi/local/social/network/activities/NewEventActivity.java
@@ -1,23 +1,12 @@
 package fi.local.social.network.activities;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Button;
-
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.Toast;
 import fi.local.social.network.R;
 import fi.local.social.network.db.Event;
@@ -26,18 +15,13 @@ import fi.local.social.network.db.EventsDataSource;
 
 public class NewEventActivity extends Activity {
 
-	private static final int SELECT_PICTURE = 1;
 	private EditText title = null;
 	private EditText content = null;
-	private ImageView image = null;
-	private Button chooseImageBtn = null;
 	private Button sendBtn = null;
-	private Bitmap bitmap = null;
 	private EventsDataSource eventsDataSource;
-	private Uri selectedImage;
+
 	private String sTitle;
 	private String sContent;
-	private String sUri;
 	private long startTime;
 	private long endTime;
 
@@ -47,40 +31,15 @@ public class NewEventActivity extends Activity {
 		setContentView(R.layout.newevent);
 		title = (EditText) findViewById(R.id.newEventTitle);
 		content = (EditText) findViewById(R.id.newEventContent);
-		image = (ImageView) findViewById(R.id.imgView);
-		chooseImageBtn = (Button) findViewById(R.id.chooseImageBtn);
 		sendBtn = (Button) findViewById(R.id.sendBtn);
-		selectedImage = null;
 		sTitle = title.getEditableText().toString();
 		sContent = content.getEditableText().toString();
-		sUri = "";
 		startTime = 0L; // TODO initialize with the default value
 		endTime = 0L; // TODO initialize with the default value
 
 		// open db
 		eventsDataSource = new EventsDataSource(getApplicationContext());
 		eventsDataSource.open();
-
-		Bitmap bitmapTemp = (Bitmap) getLastNonConfigurationInstance();
-		if (bitmapTemp != null) {
-			image.setImageBitmap(bitmapTemp);
-			bitmap = bitmapTemp;
-		} else {
-			// throw new RuntimeException("Bitmap not initialized");
-		}
-
-		// When a user clicks on the 'choose a image' button, the user is
-		// directed into the media folder to choose a image
-		chooseImageBtn.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				// Launch an image choosing activity
-				Intent imageChoosingIntent = new Intent(
-						Intent.ACTION_PICK,
-						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-				startActivityForResult(imageChoosingIntent, SELECT_PICTURE);
-			}
-		});
 
 		// When a user clicks on the 'Send Event' button, the newly created
 		// event would be sent to all nearby devices
@@ -95,35 +54,24 @@ public class NewEventActivity extends Activity {
 
 				sTitle = title.getEditableText().toString();
 				sContent = content.getEditableText().toString();
-				if (selectedImage != null) {
-					sUri = selectedImage.toString();
-				}
-				if ("".equals(sTitle)) {
+
+				if (sTitle.equals("")) {
 					Toast.makeText(getApplicationContext(),
 							"Please add a title.", Toast.LENGTH_SHORT).show();
-					return;
-				} else if ("".equals(sContent)) {
+				} else if (sContent.equals("")) {
 					Toast.makeText(getApplicationContext(),
 							"Please add a content description.",
 							Toast.LENGTH_SHORT).show();
-					return;
-				}
-				// TODO check start and endtime if they are the default
-				// values????
-				else if ("".equals(sUri)) {
-
 				} else {
-					throw new RuntimeException("Uncaught if clause");
+					sendNewEvent();
 				}
-				sendNewEvent();
 			}
-
 		});
 	}
 
 	private void sendNewEvent() {
 		Event event = new EventImpl(startTime, endTime, sTitle, sContent,
-				PeopleActivity.USERNAME, sUri);
+				PeopleActivity.USERNAME,null);
 		eventsDataSource.createEntry(event.getDBString());
 
 		startActivity(new Intent(getApplicationContext(), EventsActivity.class));
@@ -147,45 +95,4 @@ public class NewEventActivity extends Activity {
 		eventsDataSource.open();
 	}
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode,
-			Intent imageReturnedIntent) {
-		super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
-
-		switch (requestCode) {
-		case SELECT_PICTURE:
-			if (resultCode == RESULT_OK) {
-				// Uri of the selected image by user
-				selectedImage = imageReturnedIntent.getData();
-				InputStream imageStream = null;
-				try {
-					imageStream = getContentResolver().openInputStream(
-							selectedImage);
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				}
-				// Get bitmap format of the selected image
-				Bitmap yourSelectedImage = BitmapFactory
-						.decodeStream(imageStream);
-				// Show the image in the ImageView
-				image.setImageBitmap(yourSelectedImage);
-				bitmap = yourSelectedImage;// Store the bitmap of image in case
-											// of screen rotation
-			} else {
-				Log.d("Picture activity resultcode other than RESULT_OK",
-						String.valueOf(resultCode));
-			}
-			break;
-		default:
-			throw new RuntimeException("Uncaught switch clause :" + requestCode);
-		}
-
-	}
-
-	// Save the bitmap representation of the image when screen rotates and
-	// restore it upon completion of rotation.
-	@Override
-	public Object onRetainNonConfigurationInstance() {
-		return bitmap;
-	}
 }
