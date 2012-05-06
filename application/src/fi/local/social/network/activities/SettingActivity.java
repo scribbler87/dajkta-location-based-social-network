f31b2c8cfb52987f58f1b1094211a0cbf9db8aa9
Antti Vuorela <antti.vuorela@iki.fi>
2012-05-06 04:08:47 +0300

    Saving user's name to the server

--- application/src/fi/local/social/network/activities/SettingActivity.java
+++ application/src/fi/local/social/network/activities/SettingActivity.java
@@ -1,16 +1,34 @@
 package fi.local.social.network.activities;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import com.example.android.actionbarcompat.ActionBarActivity;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,6 +38,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 import fi.local.social.network.R;
+import fi.local.social.network.activities.PeopleActivity.HTTPNameRequest;
 import fi.local.social.network.btservice.BTService;
 import fi.local.social.network.db.ChatMessage;
 import fi.local.social.network.db.ChatMessagesDataSource;
@@ -28,6 +47,9 @@ import fi.local.social.network.db.UserDataSource;
 import fi.local.social.network.db.UserImpl;
 
 public class SettingActivity extends ActionBarActivity {
+	
+	private static String AddUserURL= "http://www.vugi.iki.fi/msp-api/addUser.php";
+	
 	private static final int SELECT_PICTURE = 1;
 	private ImageView image;
 	// private Button selectPic;
@@ -156,12 +178,20 @@ public class SettingActivity extends ActionBarActivity {
 		Toast.makeText(getApplicationContext(),
 				"Your nickname is " + newUsername, Toast.LENGTH_SHORT).show();
 
-		// Save the new comment to the database
-		// TODO add own device address
-		User newUser = new UserImpl(newUsername, "not yet here",
-				"dummy adrress");
+		// Get the own device address
+		BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+		String btAddress = btAdapter.getAddress();
+		Toast.makeText(getApplicationContext(),
+				"Your BT address is " + btAddress, Toast.LENGTH_SHORT).show();
+		
+		// Save user information to server
+		new HTTPSaveSettings().execute(btAddress,newUsername);
+		
+		// Save the new user to the database
+		User newUser = new UserImpl(newUsername, "", btAddress);
 		newUser.setIsPhoneUser(true);
 
+		// Remove old user
 		List<User> allEntries = userDataSource.getAllEntries();
 		User oldUser = null;
 		for (User actUser : allEntries) {
@@ -171,6 +201,7 @@ public class SettingActivity extends ActionBarActivity {
 			}
 		}
 
+		// Update chat messages
 		chatMessDataSource = new ChatMessagesDataSource(this);
 		chatMessDataSource.open();
 		List<ChatMessage> chatMessages = chatMessDataSource.getAllEntries();
@@ -192,6 +223,69 @@ public class SettingActivity extends ActionBarActivity {
 		userDataSource.createEntry(newUser.getDBString());
 
 		startActivity(new Intent(getApplicationContext(), PeopleActivity.class));
+	}
+	
+class HTTPSaveSettings extends AsyncTask<String, String, String>{
+		
+		
+		private String address;
+		private String name;
+		private String TAG = "HTTPSaveSettings";
+
+	    @Override
+	    protected String doInBackground(String... params) {
+	    	
+	    	this.address = params[0];
+	    	this.name = params[1];
+	    	Log.d(TAG,"Starting request for address: " + address);
+	    	
+	        HttpClient httpclient = new DefaultHttpClient();
+	        HttpPost httppost = new HttpPost(SettingActivity.AddUserURL);
+	        
+	        // Add your data
+	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+	        nameValuePairs.add(new BasicNameValuePair("address", address));
+	        nameValuePairs.add(new BasicNameValuePair("name", name));
+	        try {
+				httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        
+	        HttpResponse response;
+	        String responseString = null;
+	        try {
+	            response = httpclient.execute(httppost);
+	            StatusLine statusLine = response.getStatusLine();
+	            
+	            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+	                ByteArrayOutputStream out = new ByteArrayOutputStream();
+	                response.getEntity().writeTo(out);
+	                out.close();
+	                responseString = out.toString();
+	            } else{
+	                response.getEntity().getContent().close();
+	                throw new IOException(statusLine.getReasonPhrase());
+	            }
+	        } catch (IOException e) {
+	        	Log.e(TAG,"Got IOException");
+	            //TODO Handle problems..
+	        }
+	        return responseString;
+	    }
+
+	    @Override
+	    protected void onPostExecute(String result) {
+	        super.onPostExecute(result);
+	        if (result != null){
+	        	String msg = "Name saved to server: " + name;
+	        	Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+	        	Log.d(TAG,msg);
+	        	Log.d(TAG,result);
+	        }
+	        
+	    }
 	}
 	
 }
